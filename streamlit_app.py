6b5f133c09833cb4bcb1b5ae4b7374c1113cfc10
Xanistro <xanistr00@gmail.com>
2025-10-16 13:45:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,5 +17,13 @@ st.write(
 st.write(
   "attempting to make a BMI calculator"
 )
-weight=st.number_input("Weight (lbs)", min_value=0, max_value=1000, value=0)
-height=st.number_input("Height (In)", min_value=0, max_value=100, value=0)
+weight=st.number_input("Weight (kg)", min_value=0, max_value=1000, value=0)
+height=st.number_input("Height (cm)", min_value=0, max_value=100, value=0)
+
+if st.button("Calculate BMI"):
+  if weight>0 and height>0:
+    calculated_bmi=weight/((height/100)**2
+    st.success(f"Your caluclated BMI is  **{calculated_bmi:.2f}**")
+    bmi=calculated_bmi
+       else:
+                           st.warning("Please enter both weight and height to calculate for BMI")
