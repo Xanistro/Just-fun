033d43d5a18a1bd5e7ce3f3d82cf88b6245e511d
Xanistro <xanistr00@gmail.com>
2025-10-16 14:22:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Click the button to increase yoour score")
+st.title("Click the button to increase your score")
 
 if "score" not in st.session_state:
   st.session_state.score=0
@@ -8,5 +8,5 @@ if "score" not in st.session_state:
 st.write(f"**Your current score:** {st.session_state.score}")
 
 if st.button("Increase Score"):
-    st.session_state.score += 1
+    st.session_state.score + 1
 
