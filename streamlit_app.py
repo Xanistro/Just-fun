91adb1de9ceaefcf6b72d3cc57d6343c215b916b
Xanistro <xanistr00@gmail.com>
2025-10-16 14:19:51 -0500

    Change initial score to -1

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("Click the button to increase yoour score")
 
 if "score" not in st.session_state:
-  st.session_state.score=0
+  st.session_state.score=-1
 
 st.write(f"**Your current score:** {st.session_state.score}")
 
