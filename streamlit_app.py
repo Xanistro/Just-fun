826ac1c7e0998e64111bc2c867ab0e07b00799df
Xanistro <xanistr00@gmail.com>
2025-10-16 13:28:24 -0500

    Change title from 'Diddy' to 'Hello, World'

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
-st.title("Diddy")
+st.title("Hello, World")
 st.write(
   "I wrote this to learn python if it works. "
   "W's in the chat"
 )
 
 st.number_input("How happy are you?", min_value=0, max_value=10, value=5)
+st.number_input("How sad are you?", min_value=0, max_value=10, value=5)
